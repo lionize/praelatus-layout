50d084062a4bc086cdf7c45e97c7834c9b1e376a
Mark Chandler <mark.allen.chandler@gmail.com>
2017-04-19 20:18:52 -0400

    Create TicketListItem component

--- src/TicketList.js
+++ src/TicketList.js
@@ -1,50 +1,15 @@
 import React from 'react';
 import glamorous from 'glamorous';
-const { Div } = glamorous;
-import ListItemIcon from './ListItemIcon'
+import TicketListItem from './TicketListItem'
 
 const TicketList = glamorous.div((props, theme) => ({
   display: 'flex',
   flexDirection: 'column',
 }));
 
-const ListItem = glamorous.div((props, theme) => ({
-  flex: '1 1 auto',
-  display: 'flex',
-  alignItems: 'stretch',
-  backgroundColor: props.active ? 'rgba(0,0,0,0.040)' : 'inherit',
-}));
-
-const ItemLink = glamorous.a((props, theme) => ({
-  fontSize: 'inherit',
-  fontWeight: 'inherit',
-  textDecoration: 'none',
-  padding: '0.5em 1em 0.5em 0.5rem',
-  color: 'inherit',
-  flex: '1',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'flex-start',
-}));
-
-const Title = glamorous.div((props, theme) => ({}));
-
-const Summary = glamorous.div((props, theme) => ({
-  fontSize: '0.8em',
-}));
 
 export default ({ tickets = [] }) => (
   <TicketList>
-    {tickets.map(ticket => (
-      <ListItem active={ticket.id === 1} key={ticket.id}>
-        <ItemLink href="#">
-          <ListItemIcon priority={ticket.priority} />
-          <Div padding='0 0.5rem 0 0.5rem'>
-            <Title>{ticket.key}</Title>
-            <Summary>{ticket.summary}</Summary>
-          </Div>
-        </ItemLink>
-      </ListItem>
-    ))}
+    {tickets.map(ticket => <TicketListItem key={ticket.id} ticket={ticket} />)}
   </TicketList>
 );
---
+++ src/TicketListItem.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import glamorous from 'glamorous'
+const { Div } = glamorous
+import ListItemIcon from './ListItemIcon'
+
+const Title = glamorous.div((props, theme) => ({
+  fontSize: '1.1em',
+}));
+
+const Summary = glamorous.div((props, theme) => ({
+  fontSize: '0.8em',
+}));
+
+const ListItem = glamorous.div((props, theme) => ({
+  flex: '1 1 auto',
+  display: 'flex',
+  alignItems: 'stretch',
+  backgroundColor: props.active ? 'rgba(0,0,0,0.040)' : 'inherit',
+}));
+
+const ItemLink = glamorous.a((props, theme) => ({
+  fontSize: 'inherit',
+  fontWeight: 'inherit',
+  textDecoration: 'none',
+  padding: '0.5em 1em 0.5em 0.5rem',
+  color: 'inherit',
+  flex: '1',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'flex-start',
+}));
+
+const TicketListItem = ({ ticket: { id, key, priority, summary }}) => (
+  <ListItem active={id === 1} key={id}>
+    <ItemLink href="#">
+      <ListItemIcon priority={priority} />
+      <Div padding='0 0.5rem 0 0.5rem'>
+        <Title>{key}</Title>
+        <Summary>{summary}</Summary>
+      </Div>
+    </ItemLink>
+  </ListItem>
+)
+
+export default TicketListItem
