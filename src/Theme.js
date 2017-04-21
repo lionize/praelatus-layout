30c6abd29e96766f39bd01ea4aa5b265ca5c0105
Mark Chandler <mark.allen.chandler@gmail.com>
2017-04-21 17:45:50 -0400

    Create basic color scheme in Theme

--- src/Theme.js
+++ src/Theme.js
@@ -7,6 +7,11 @@ const Theme = {
     light: '1px solid rgba(0,0,0,0.1)',
     dark: '1px solid rgba(0,0,0,0.2)',
   },
+  colors: {
+    light: 'rgba(0,0,0,0.4)',
+    medium: 'rgba(0,0,0,0.55)',
+    dark: 'rgba(0,0,0,0.7)',
+  }
 };
 
 export default Theme;
